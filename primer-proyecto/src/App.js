bc24ab795f34b1b833ecd91542da6842b64a5447
Luis fernando vega <lfexevega95@gmail.com>
2022-04-07 21:03:03 -0300

    segundo commit

--- primer-proyecto/src/App.js
+++ primer-proyecto/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Felicidades, es el visitante numero 1 millon
         </p>
         <a
           className="App-link"
